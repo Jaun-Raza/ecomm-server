c4cd4575133f188b2ac442d0ef3610bfcdc8c904
Developer <106108668+Jaun-Raza@users.noreply.github.com>
2023-10-19 23:12:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(session({
     saveUninitialized: true,
     secure: true, // or remove this line
     cookie: {
-         // domain: "https://ecommserver-pado7k34.b4a.run/",
+        domain: "https://client-kappa-rouge-53.vercel.app",
         maxAge: 1000 * 60 * 60 * 24, // Set your desired expiration time
     },
 }));
